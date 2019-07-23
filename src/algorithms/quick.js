22a436b77f52ea38baf20a39df3258fdc466a469
changzhn <changzhenan0212@gmail.com>
2019-07-23 14:15:01 +0800

    update readme

--- src/algorithms/quick.js
+++ src/algorithms/quick.js
@@ -1,5 +1,12 @@
 
 /**
+ 1. 数组左侧第一项作为基准数，设置左右两个指针从两头开始向中间靠拢（右侧指针先移动），如果右侧指针找到数字比基准数小则停止，如果左侧指针找到数字比基准数大则停止，交换左右指针的数字；
+ 2. 如果左右指针指向同一个位置，则交换该位置与基准数字位置的数字；
+ 3. 第一轮循环结束，此时，基准数字左侧都比基准数字小，右侧都比基准数大；
+ 4. 递归排列左侧的数组和右侧的数组；
+ */
+
+/**
  *
  * @param {Array} arr
  */
@@ -12,6 +19,7 @@ function quick(arr) {
 	 * @param {Number} rightIdx
 	 */
 	function innerQuick(arr, leftIdx, rightIdx) {
+		/* 1 */
 		let base = arr[leftIdx];
 		let i = leftIdx;
 		let j = rightIdx;
@@ -35,8 +43,10 @@ function quick(arr) {
 			}
 		}
 
+		/* 2 */
 		[arr[leftIdx], arr[i]] = [arr[i], base];
 
+		/* 4 */
 		innerQuick(arr, leftIdx, i - 1);
 		innerQuick(arr, i + 1, rightIdx);
 	}
