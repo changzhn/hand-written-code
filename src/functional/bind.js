70a22e4e0cacaf20d3bf243a50bfff90c162065f
changzhn <changzhenan0212@gmail.com>
2019-07-23 17:00:22 +0800

    bind

--- src/functional/bind.js
+++ src/functional/bind.js
@@ -1,9 +1,13 @@
 
+function apply(fn, context, args) {
+	context.fn = fn;
+	return context.fn(...args);
+}
 
 Function.prototype.bind2 = function(context, ...args1) {
 	context = context || window;
 	const fn = this;
 	return function(...args2) {
-		fn.apply(context, [...args1, ...args2])
+		return apply(fn, context, [...args1, ...args2])
 	}
 }
