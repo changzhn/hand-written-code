3cb4f60fa375b0b2a360b94985396f2edc836adb
changzhn <changzhenan0212@gmail.com>
2019-08-06 19:55:59 +0800

    补充求最小值题目

--- src/algorithm/getMin.js
+++ src/algorithm/getMin.js
@@ -1,7 +1,12 @@
+/**
+ * 题目：一个矩形田地，需要划分分一个个面积相等的正方形，现在需要将正方形的边长最大，求边长
+ * 或者：求两个正整数的最大公约数
+ */
+
 
 function getMin(width, height) {
 
-	if (height < 1) {
+	if (height < 1) { // 如果小于1则不往下求值
 		return null;
 	}
 
