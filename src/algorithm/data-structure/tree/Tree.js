1426423f4a528edc6b7943f867210921c1b53335
changzhn <377757708@qq.com>
2019-09-30 14:50:03 +0800

    tree

--- src/algorithm/data-structure/tree/Tree.js
+++ src/algorithm/data-structure/tree/Tree.js
@@ -1,123 +1,36 @@
-function Node(data, left, right) {
-    this.data = data;
-    this.left = left;
-    this.right = right;
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
 }
 
-Node.prototype = {
-    show: function () {
-        console.log(this.data);
-    }
-}
+class Tree {
 
-function Tree() {
-    this.root = null;
-    this.queue = [];
-}
-
-Tree.prototype = {
-    insert: function (data) {
-        var node = new Node(data, null, null);
-        if (!this.root) {
-            this.root = node;
-            return;
-        }
-        var current = this.root;
-        var parent = null;
-        while (current) {
-            parent = current;
-            if (data < parent.data) {
-                current = current.left;
-                if (!current) {
-                    parent.left = node;
-                    return;
-                }
-            } else {
-                current = current.right;
-                if (!current) {
-                    parent.right = node;
-                    return;
-                }
-            }
-
-        }
-    },
-    preOrder: function (node) {
-        if (node) {
-            node.show();
-            this.preOrder(node.left);
-            this.preOrder(node.right);
-        }
-    },
-    middleOrder: function (node) {
-        if (node) {
-            this.middleOrder(node.left);
-            node.show();
-            this.middleOrder(node.right);
-        }
-    },
-    laterOrder: function (node) {
-        if (node) {
-            this.laterOrder(node.left);
-            this.laterOrder(node.right);
-            node.show();
-        }
-    },
-    bfc: function() {
-        this.queue = [this.root];
-        while(this.queue.length) {
-            let node = this.queue.shift();
-            if (node) {
-                console.log(node.data);
-                this.queue.push(node.left, node.right)
-            }
-        }
-    },
-    getMin: function () {
-        var current = this.root;
-        while(current){
-            if(!current.left){
-                return current;
-            }
-            current = current.left;
-        }
-    },
-    getMax: function () {
-        var current = this.root;
-        while(current){
-            if(!current.right){
-                return current;
-            }
-            current = current.right;
-        }
-    },
-    getDeep: function (node,deep) {
-        deep = deep || 0;
-        if(node == null){
-            return deep;
-        }
-        deep++;
-        var dleft = this.getDeep(node.left,deep);
-        var dright = this.getDeep(node.right,deep);
-        return Math.max(dleft,dright);
+  insert(val) {
+    if (!this.root) {
+      this.root = new Node(val);
+      return;
     }
-}
+    let current = this.root;
 
-Tree.reConstructBinaryTree = function reConstructBinaryTree(pre, vin) {
-    if(pre.length === 0){
-        return null;
+    while(current) {
+      if (val < current.val) {
+        if (current.left) {
+          current = current.left;
+        } else {
+          current.left = new Node(val);
+          return;
+        }
+      } else if (val > current.val) {
+        if (current.right) {
+          current = current.right;
+        } else {
+          current.right = new Node(val);
+          return;
+        }
+      }
     }
-    if(pre.length === 1){
-        return new Node(pre[0]);
-    }
-    const value = pre[0];
-    const index = vin.indexOf(value);
-    const vinLeft = vin.slice(0,index);
-    const vinRight = vin.slice(index+1);
-    const preLeft = pre.slice(1,index+1);
-    const preRight = pre.slice(index+1);
-    const node = new Node(value);
-    node.left = reConstructBinaryTree(preLeft, vinLeft);
-    node.right = reConstructBinaryTree(preRight, vinRight);
-    return node;
+  }
 }
--- src/algorithm/data-structure/tree/tree-test.js
+++ src/algorithm/data-structure/tree/tree-test.js
@@ -7,16 +7,4 @@ t.insert(5);
 t.insert(7);
 t.insert(6);
 t.insert(0);
-console.log(t);
-// // t.middleOrder(t.root);
-// console.log(t.getMin(), t.getMax());
-// // console.log(t.getDeep(t.root, 0));
-// // console.log(t.getNode(5,t.root));
-
-// let max = t.getMax();
-// console.log('max:', max)
-
-// t.bfc();
-
-// t.laterOrder(t.root)
-console.log(t.getDeep(t.root));
+console.log(JSON.stringify(t));
